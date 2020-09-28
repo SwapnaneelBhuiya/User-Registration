4325033559f3f0d8f37338cbdb47e32df70e1ff0
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-09-28 13:31:27 +0530

    [swapnaneel]UC4:Validate proper mobile number

--- src/com/practice/userregistration/UserReg.java
+++ src/com/practice/userregistration/UserReg.java
@@ -7,7 +7,7 @@ package com.practice.userregistration;
 			Scanner sc=new Scanner(System.in);
 			System.out.println("Enter First Name");
 			String first=sc.nextLine();
-			if(first.matches("^[A-Z]{1}[a-z]{2,}"))
+			if(validFirst(first))
 			{
 				System.out.println("Valid Name");
 			}
@@ -18,7 +18,7 @@ package com.practice.userregistration;
 			}
 			System.out.println("Enter Last Name");
 			String last=sc.nextLine();
-			if(last.matches("^[A-Z]{1}[a-z]{2,}"))
+			if(validLast(last))
 			{
 				System.out.println("Valid Name");
 			}
@@ -29,11 +29,59 @@ package com.practice.userregistration;
 			}
 			System.out.println("Enter email address");
 			String email=sc.nextLine();
-			if(email.matches("^(abc)[_+.-]{0,1}[a-zA-Z]*[@]{1}(bridgelabz|gmail|yahoo)[.]{1}(co|com|net)[.]{0,1}[a-z]{0,2}"))
+			if(validEmail(email))
 				System.out.println("valid email");
 			else
 			{
 				System.out.println("Invalid Email");System.exit(0);
+			}
+			System.out.println("Enter mobile number");
+			String number=sc.nextLine();
+			if(validNumber(number))
+				System.out.println("Valid phone number");
+			else
+			{
+				System.out.println("Invalid number");System.exit(0);
+			}
+		}
+		public static boolean validNumber(String number)
+		{
+			if(number.matches("^[0-9]{2}\\s{1}[1-9]{1}[0-9]{9}"))
+				return true;
+			else
+				return false;
+		}
+		public static boolean validFirst(String first)
+		{
+			if(first.matches("^[A-Z]{1}[a-z]{2,}"))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+		public static boolean validLast(String last)
+		{
+			if(last.matches("^[A-Z]{1}[a-z]{2,}"))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+		public static boolean validEmail(String email)
+		{
+			if(email.matches("^(abc)[_+.-]{0,1}[a-zA-Z]*[@]{1}(bridgelabz|gmail|yahoo)[.]{1}(co|com|net)[.]{0,1}[a-z]{0,2}"))
+				return true;			
+			else
+			{
+				return false;
 			}
 		}
 
