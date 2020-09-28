01a6ce7e2f47d02ce9360f80086ea0630350fe49
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-09-28 13:00:19 +0530

    [swapnaneel]UC1:Check validity for first name

---
+++ src/com/practice/userregistration/UserReg.java
@@ -0,0 +1,20 @@
+package com.practice.userregistration;
+import java.util.*;
+public class UserReg {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter First Name");
+		String first=sc.nextLine();
+		if(first.matches("^[A-Z]{1}[a-z]{2,}"))
+		{
+			System.out.println("Valid Name");
+		}
+		else
+		{
+			System.out.println("Invalid first name");
+			System.exit(0);;
+		}
+	}
+
+}
